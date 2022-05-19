bf83ca1ff64fabde73c1866fce543326c9b99c95
vishal-bypt <87118610+vishal-bypt@users.noreply.github.com>
2022-05-19 19:04:12 +0530

    Initial Commit

---
+++ ApiGateway/index.js
@@ -0,0 +1,24 @@
+﻿const gateway = require("fast-gateway");
+const server = gateway({
+  routes: [
+    {
+      prefix: "/order",
+      target: "http://localhost:8081",
+    },
+    {
+      prefix: "/payment",
+      target: "http://localhost:8082",
+    },
+  ],
+});
+
+
+server.get("/", (req, res) => {
+  res.send("Welcome to API Gateway");
+});
+
+
+
+server.start(9002).then(server => {
+  console.log("API Gateway running on 9001");
+});
---
+++ Order/index.js
@@ -0,0 +1,34 @@
+﻿const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 8081;
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+app.set('port', PORT);
+app.set('env', NODE_ENV);
+app.use(bodyParser.json());
+
+
+app.get("/", (req, res) => {
+  const response = [
+    {
+      id : 1,
+      name : "order 1"
+    },
+    {
+      id : 2,
+      name : "order 2"
+    }
+  ];
+  res.status(200).json({status : 200, response :response});
+})
+
+
+app.listen(PORT, () => {
+  console.log(
+    `Express Server started on Port ${app.get(
+      'port'
+    )} | Environment : ${app.get('env')}`
+  );
+});
